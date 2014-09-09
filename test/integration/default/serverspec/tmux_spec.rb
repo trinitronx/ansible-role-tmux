db215416ced0e415ba02a326e4ad6d361c9b143f
James Cuzella <james.cuzella@lyraphase.com>
2014-09-08 18:17:47 -0600

    Adding serverspec tests for role

---
+++ test/integration/default/serverspec/tmux_spec.rb
@@ -0,0 +1,20 @@
+require 'spec_helper'
+
+describe command('which tmux') do
+  its(:stdout) { should match /tmux$/ }
+  its(:exit_status) { should eq 0 }
+end
+
+describe file('/usr/bin/tmux') do
+  it { should be_file }
+  it { should be_executable }
+  it { should be_executable.by('others') }
+#  it { should be_linked_to '../Cellar/tmux/1.9a/bin/tmux' } ## only on OSX with homebrew...
+  it { should be_owned_by 'root' }
+  it { should be_grouped_into 'root' }
+end
+
+#describe file('/usr/local/etc/wemux.conf') do
+#  it { should be_file }
+#  its(:content) { should match /^host_list=\(\s*root\s*\)/ }
+#end
