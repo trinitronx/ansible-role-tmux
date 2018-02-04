c016fc2826822a4a1d40321dd006a6638bc2ef2b
James Cuzella <james.cuzella@lyraphase.com>
2018-02-04 01:59:18 -0700

    Really fix role name in CI helper script

--- test/integration/helpers/get-dependencies.sh
+++ test/integration/helpers/get-dependencies.sh
@@ -17,8 +17,8 @@ fi
 
 #[ ! -d $testhelpersdir/juju4.redhat-epel ] && git clone https://github.com/juju4/ansible-redhat-epel $testhelpersdir/juju4.redhat-epel
 ## galaxy naming: kitchen fails to transfer symlink folder
-#[ ! -e $testhelpersdir/trinitronx.tmux ] && ln -s ansible-role-wemux $testhelpersdir/trinitronx.tmux
-[ ! -e $testhelpersdir/trinitronx.tmux ] && cp -R $testhelpersdir/ansible-role-wemux $testhelpersdir/trinitronx.tmux
+#[ ! -e $testhelpersdir/trinitronx.tmux ] && ln -s ansible-role-tmux $testhelpersdir/trinitronx.tmux
+[ ! -e $testhelpersdir/trinitronx.tmux ] && cp -R $testhelpersdir/ansible-role-tmux $testhelpersdir/trinitronx.tmux
 
 ## don't stop build on this script return code
 true
